753ce422a7eb89d65cf8388678095f7bd90aac04
Obaidullah Ahmadzai <95979473+obaidahmadzai@users.noreply.github.com>
2023-07-07 23:58:25 +0430

    feat: regular update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+ app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Pr API')
@@ -12,7 +18,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('PR')
     .build();
+
+
   const document = SwaggerModule.createDocument(app, config);
+
+
   SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
